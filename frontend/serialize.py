485afce7f7828cdf27c5e6bdd4c825c8f1dfb3ac
Martin Ennemoser <martin.ennemoser@jku.at>
2017-11-21 10:49:17 +0100

    Implemented storing/loading of instances

--- frontend/serialize.py
+++ frontend/serialize.py
@@ -2,6 +2,9 @@ import csv
 import os
 import json
 
+foldername = 'data'
+filename = 'foo.json'
+
 def createJSON(images, heatmaps, labels):
     instances = []
     for i in range(0, len(images)):
@@ -16,13 +19,16 @@ def createDataFolder(foldername):
     if not os.path.exists(foldername):
         os.makedirs(foldername)
 
-def serialize(a, b, c):
-    if len(a) != len(b) and len(b) != len(c):
+def serialize(images, heatmaps, labels):
+    if len(images) != len(heatmaps) and len(heatmaps) != len(labels):
         raise ValueError('The dimensions of the input/activities/labels are not compatible')
-    foldername = 'data'
-    filename = 'foo.json'
     createDataFolder(foldername)
 
 
     with open(foldername + '/' + filename, 'w') as f:
-       json.dump(createJSON(a, b, c), f)
+       json.dump(createJSON(images, heatmaps, labels), f)
+
+def deserialize():
+    with open(foldername + '/' + filename, 'r') as f:
+        return json.load(f)
+
--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from frontend.data_loader import loadDataSet
 from frontend.plot import View
 from frontend.lrp_executor import applyLRP
-from frontend.serialize import serialize
+from frontend.serialize import serialize, deserialize
 from sklearn.utils import shuffle
 
 import argparse
+import numpy as np
 def firstFunctionEver():
     loadData()
 
 def loadData():
     return loadDataSet('MNIST');
 
+def parseArguments():
+    parser = argparse.ArgumentParser(description='Creates projections for input images and LRP activations')
+    parser.add_argument('integers', metavar='N', type=int, nargs='?',
+                        help='number of images to process (12...10000)')
+    args = parser.parse_args()
+    return args
+
 if __name__ == '__main__':
-    generate = True
-    if generate:
-        instanceCount = 12
+    args = parseArguments()
+    if args.integers:
+        instanceCount = args.integers
+        if instanceCount < 12 or instanceCount > 10000:
+            raise ValueError('The instance count is out of range')
         data = loadData()
         X_train, y_train = data.data[:70000] / 255., data.target[:70000]
         X_train, y_train = shuffle(X_train, y_train)
@@ -22,14 +32,17 @@ if __name__ == '__main__':
         images = X_train[:instanceCount]
         labels = y_train[:instanceCount]
 
-        view = View()
-
-        view.plotInput(images, labels, instanceCount)
-
         heatmaps = applyLRP(images, labels, instanceCount)
         serialize(images, heatmaps, labels)
-        view.plotActivations(heatmaps, labels, instanceCount)
-
-        view.show()
     else:
-        print('load')
+        x = deserialize()
+        instanceCount = len(x)
+        images = np.array(list((map(lambda y: y['input_image'] , x))))
+        heatmaps = np.array(list(map(lambda y: y['heatmap'] , x)))
+        labels = np.array(list(map(lambda y: y['label'] , x)))
+
+        view = View()
+        view.plotInput(images, labels, instanceCount)
+        view.plotActivations(heatmaps, labels, instanceCount)
+        view.show()
+
